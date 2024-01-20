07cb1509dba22fbdc7aeea8a4872731c8411f476
unknown <tristandonnally@gmail.com>
2024-01-20 01:08:38 -0500

    adjustments to nav styling, scroll behavior added to nav background, changed nav size on desktop

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,41 +1,18 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
-    /*const sections = document.querySelectorAll("body>div");
-    let currentSection = 1;
-    sections[currentSection].scrollIntoView();
-    let lastScrollTop = window.scrollY;
-    let paused = false;
+window.onscroll = handleScroll;
 
-    window.addEventListener("scroll", (event)=>{
-        if (!paused) {
-            let st = window.scrollY;
-            console.log("st: ", st);
-            console.log("LastScrollTop: ", lastScrollTop);
-        
-            // Determine the scroll direction
-            if (st > lastScrollTop) {
-              // Scrolling down
-              console.log("Scrolling Down");
-              currentSection = Math.min(currentSection + 1, sections.length);
-              sections[currentSection-1].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-            } else if(st < lastScrollTop) {
-              // Scrolling up
-              console.log("Scrolling Up");
-              currentSection = Math.max(currentSection - 1, 1);
-            }
-        
-            
-        
-            // Pause scrolling temporarily to prevent multiple rapid scrolls
-            paused = true;
-            setTimeout(() => {
-                paused = false;
-                lastScrollTop = window.scrollY;
-              // Update the last scroll position
+function handleScroll() {
+  const navbar = document.querySelector('.navbar');
+  let scrollFromTop = window.scrollY || window.scrollY;
 
-            }, 500); // Adjust the delay as needed
-          }
-        else{
-            event.preventDefault;
-        }
-    })*/
-})
+  // Adjust the scroll threshold as needed
+  let scrollThreshold = 100;
+
+  // Check if the user has scrolled beyond the threshold
+  if (scrollFromTop > scrollThreshold) {
+    // Add the class when scrolled
+    navbar.classList.add('scrolled-nav');
+  } else {
+    // Remove the class when not scrolled
+    navbar.classList.remove('scrolled-nav');
+  }
+}
